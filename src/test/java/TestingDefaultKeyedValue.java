c78cb9f85f111a0b0fa515613adcc3e4865cf0e2
marwan harb <marwanharb65@outlook.com>
2020-03-28 19:22:01 +0200

    Imported files

---
+++ src/test/java/TestingDefaultKeyedValue.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingDefaultKeyedValue {
+
+	@Test
+	public void testHashCode() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testDefaultKeyedValue() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetKey() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetValue() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSetValue() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testEqualsObject() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testClone() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testToString() {
+		assertTrue(true);
+	}
+
+}
